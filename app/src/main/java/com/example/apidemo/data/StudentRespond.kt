0349cbca936818d90ef3db0697fcbf2376110567
tingchingjong <reksor9087@gmail.com>
2021-09-01 11:51:02 +0800

    Initial commit

---
+++ app/src/main/java/com/example/apidemo/data/StudentRespond.kt
@@ -0,0 +1,6 @@
+package com.example.apidemo.data
+
+data class StudentRespond(val id:String,
+                          val name:String,
+                          val programme:String,
+                          val imgURL:String)
